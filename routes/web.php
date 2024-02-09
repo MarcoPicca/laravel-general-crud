d6a938148b058b041c5fc645e318372135dd1b33
MarcoPicca <il96marco@gmail.com>
2024-02-09 16:15:40 +0100

    inizio git branch

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,3 @@ use App\Http\Controllers\Guest\PokemonController as GuestPokemonController;
 Route::get('/', function () { return view('layouts.app'); });
 
 Route::get('/pokemons', [GuestPokemonController::class, 'index'])->name('guest.pokemons.index');
-Route::post('/pokemons', [GuestPokemonController::class, 'store'])->name('guest.pokemons.store');
-Route::get('/pokemons/create', [GuestPokemonController::class, 'create'])->name('guest.pokemons.create');
-Route::get('/pokemons/{pokemon}', [GuestPokemonController::class, 'show'])->name('guest.pokemons.show');
-Route::put('/pokemons/{pokemon}', [GuestPokemonController::class, 'update'])->name('guest.pokemons.update');
-Route::get('/pokemons/{pokemon}/edit', [GuestPokemonController::class, 'edit'])->name('guest.pokemons.edit');
